d7617015cbbe14fa115d8fc53c1a3dc70e1d6853
Thomas Micol <thomasmicol@gmail.com>
2020-12-15 21:13:31 +1300

    added auth provider

--- Auth0 Integration Demo/ClientApp/src/index.tsx
+++ Auth0 Integration Demo/ClientApp/src/index.tsx
@@ -8,10 +8,12 @@ import { createBrowserHistory } from 'history';
 import configureStore from './store/configureStore';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Auth0Provider } from "@auth0/auth0-react"; 
 
 // Create browser history to use in the Redux store
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href') as string;
 const history = createBrowserHistory({ basename: baseUrl });
+
 
 // Get the application-wide store instance, prepopulating with state from the server where available.
 const store = configureStore(history);
@@ -19,9 +21,15 @@ const store = configureStore(history);
 ReactDOM.render(
     <Provider store={store}>
         <ConnectedRouter history={history}>
-            <App />
+            <Auth0Provider
+                clientId="QxBmWM7872ZModNxHqEchez3eUGsONId"
+                domain="dev-21y0dwga.au.auth0.com"
+                redirectUri={window.location.origin}>
+                <App />
+            </Auth0Provider>
         </ConnectedRouter>
     </Provider>,
     document.getElementById('root'));
 
 registerServiceWorker();
+
