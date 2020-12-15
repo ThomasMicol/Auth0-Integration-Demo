8cb6b28a35506c5eedaf5bc976879b99cb0f34ca
Thomas Micol <thomasmicol@gmail.com>
2020-12-15 21:12:43 +1300

    added login component

---
+++ Auth0 Integration Demo/ClientApp/src/components/Login.tsx
@@ -0,0 +1,17 @@
+﻿import * as React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const Login = () => {
+    const { loginWithRedirect, isAuthenticated, logout, isLoading } = useAuth0();
+    const returnUrl = window.location.host;
+    return (<>
+        {isLoading ? <></> : 
+            isAuthenticated
+                ?<button onClick = { () => logout({ returnTo: returnUrl }) }>Logout</button>
+            : <button onClick={() => { loginWithRedirect() }}>Login</button>
+        }
+        
+    </>);
+}
+
+export default Login;
